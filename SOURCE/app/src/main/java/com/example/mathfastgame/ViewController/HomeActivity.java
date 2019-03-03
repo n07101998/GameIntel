2c3774e9390f2deb3e0ff377f3625e222e93efa7
caoson2798 <ygiangson@gmail.com>
2019-03-03 11:35:11 +0700

    update UI

--- SOURCE/app/src/main/java/com/example/mathfastgame/ViewController/HomeActivity.java
+++ SOURCE/app/src/main/java/com/example/mathfastgame/ViewController/HomeActivity.java
@@ -1,16 +1,39 @@
 package com.example.mathfastgame.ViewController;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.mathfastgame.R;
 import com.example.mathfastgame.ViewController.Base.BaseActivity;
 
 public class HomeActivity extends BaseActivity {
+    ImageButton btnPlay,btnRank,btnInfor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        init();
+        addEvents();
+    }
+
+    private void addEvents() {
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(HomeActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void init() {
+        btnInfor=findViewById(R.id.btn_infor);
+        btnPlay=findViewById(R.id.btn_play);
+        btnRank=findViewById(R.id.btn_rank);
     }
 }
--- SOURCE/app/src/main/java/com/example/mathfastgame/ViewController/MainActivity.java
+++ SOURCE/app/src/main/java/com/example/mathfastgame/ViewController/MainActivity.java
@@ -1,16 +1,60 @@
 package com.example.mathfastgame.ViewController;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import com.example.mathfastgame.R;
 import com.example.mathfastgame.ViewController.Base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
+    ProgressBar pbCount;
+    TextSwitcher txtQues;
+    TextView txtPoint;
+    ImageButton btnTrue,btnFalse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        setUpView();
+    }
+
+    private void setUpView() {
+        txtQues.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                TextView textView = new TextView(MainActivity.this);
+                textView.setTextColor(Color.WHITE);
+                textView.setTextSize(100);
+                textView.setGravity(Gravity.CENTER_HORIZONTAL);
+                return textView;
+            }
+        });
+        txtQues.setText("2+5=4");
+
+    }
+    int i=0;
+    private void init() {
+        pbCount=findViewById(R.id.pb_count);
+        txtQues=findViewById(R.id.txt_ques);
+        txtPoint=findViewById(R.id.txt_point);
+        btnFalse=findViewById(R.id.btn_false);
+        btnTrue=findViewById(R.id.btn_true);
+        btnTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ++i;
+                txtQues.setText("1+1="+i);
+            }
+        });
     }
 }
